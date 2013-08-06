536a85bbd77a1f86a7b9307cc0b892837e154e62
NiteshKumar <nitesh.pal@weboniselab.com>
2013-08-06 18:38:48 +0530

    getting multiple value from api call

--- StudentResult/StudentResult/ResultViewController.m
+++ StudentResult/StudentResult/ResultViewController.m
@@ -43,12 +43,12 @@
     int heightSpace=179;
     for(int i=0;i<[resultStudent.subjects count];i++){
         heightSpace=(30+heightSpace);
-    UILabel *lblsubjectName=[[UILabel alloc]initWithFrame:CGRectMake(27, heightSpace, 126, 21)];
+    UILabel *lblsubjectName=[[UILabel alloc]initWithFrame:CGRectMake(27, heightSpace, 175, 21)];
         //NSArray *subArr=[NSArray arrayWithObject:resultStudent.subjects];
         Subject *sub=[resultStudent.subjects objectAtIndex:i];
         lblsubjectName.text=sub.subject;
         [[self view] addSubview:lblsubjectName];
-    UILabel *lblMarks=[[UILabel alloc]initWithFrame:CGRectMake(180, heightSpace, 126, 21)];
+    UILabel *lblMarks=[[UILabel alloc]initWithFrame:CGRectMake(180, heightSpace, 175, 21)];
         lblMarks.text=[sub.mark stringValue];
         [[self view] addSubview:lblMarks];
     }
